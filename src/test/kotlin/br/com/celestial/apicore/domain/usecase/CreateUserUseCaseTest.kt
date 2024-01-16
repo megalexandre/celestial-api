4c0b8537b5393ee7440aec44d21464652488ca03
Alexandre Queiroz <megalexandre@gmail.com>
2024-01-16 14:39:29 -0300

    CostCenter

--- src/test/kotlin/br/com/celestial/apicore/domain/usecase/CreateUserUseCaseTest.kt
+++ src/test/kotlin/br/com/celestial/apicore/domain/usecase/CreateUserUseCaseTest.kt
@@ -5,29 +5,16 @@ import br.com.celestial.apicore.domain.exception.InvalidUsecaseException
 import br.com.celestial.apicore.domain.usecase.user.CreateUserUseCase
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import stubs.entity.userStub
+
 
 class CreateUserUseCaseTest {
 
     private val dataSource = mockk<UserDataSource>()
     private val useCase = CreateUserUseCase(dataSource = dataSource)
-
-    @Test
-    fun `GIVEN a new user is created WHEN document number already exists THEN do not save him`() {
-
-        val user = userStub
-
-        every {
-            dataSource.findByDocument(user.documentNumber)
-        } returns user
-
-        assertThrows<InvalidUsecaseException> {
-            useCase.execute(user)
-        }
-
-    }
 
     @Test
     fun `GIVEN a new user is created WHEN document number not exists THEN save him`() {
@@ -42,6 +29,28 @@ class CreateUserUseCaseTest {
         } returns user
 
         useCase.execute(user)
+
+        verify {
+            dataSource.save(user)
+        }
     }
 
+    @Test
+    fun `GIVEN a new user is created WHEN document number already exists THEN do not save him`() {
+
+        val user = userStub
+
+        every {
+            dataSource.findByDocument(user.documentNumber)
+        } returns user
+
+        assertThrows<InvalidUsecaseException> {
+            useCase.execute(user)
+        }
+
+        verify (exactly = 0){
+            dataSource.save(user)
+        }
+
+    }
 }
